8d05307c6a0f99bc05c475ac25007fa1f94242e9
Tyler <ureras@gmail.com>
2017-02-19 14:46:28 -0800

    Created correctly-sized window with a menu and scroll panes

--- src/tunecomposer/TuneComposer.java
+++ src/tunecomposer/TuneComposer.java
@@ -5,12 +5,9 @@ package tunecomposer;
 
 import java.io.IOException;
 import javafx.application.Application;
-import javafx.event.ActionEvent;
-import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.TextInputDialog;
 import javafx.stage.Stage;
 import javafx.stage.WindowEvent;
 
@@ -22,76 +19,7 @@ import javafx.stage.WindowEvent;
  */
 public class TuneComposer extends Application {
     
-    /**
-     * Represents the number of pitch steps for 
-     * do, re, mi, fa, so, la, ti, do.
-     * Source: https://en.wikipedia.org/wiki/Solfège
-     */
-    private static final int[] SCALE = {0, 2, 4, 5, 7, 9, 11, 12};
-    
-    /**
-     * Play notes at maximum volume.
-     */
-    private static final int VOLUME = 127;
-    
-    /**
-     * One midi player is used throughout, so we can stop a scale that is
-     * currently playing.
-     */
-    private final MidiPlayer player;
 
-    /**
-     * Constructs a new ScalePlayer application.
-     */
-    public TuneComposer() {
-        this.player = new MidiPlayer(1,60);
-    }
-    
-    /**
-     * Play a new scale, after stopping and clearing any previous scale.
-     * @param startingPitch an integer between 0 and 115
-     */
-    protected void playScale(int startingPitch) {
-        player.stop();
-        player.clear();
-        for (int i=0; i < 8; i++) {
-            player.addNote(startingPitch+SCALE[i], VOLUME, i,    1, 0, 0);
-            player.addNote(startingPitch+SCALE[i], VOLUME, 16-i, 1, 0, 0);
-        }
-        player.play();
-    }
-    
-    /**
-     * When the user clicks the "Play scale" button, show a dialog to get the 
-     * starting note and then play the scale.
-     * @param event the button click event
-     */
-    @FXML 
-    protected void handlePlayScaleButtonAction(ActionEvent event) {
-        TextInputDialog pitchDialog = new TextInputDialog("60");
-        pitchDialog.setHeaderText("Give me a starting note (0-115):");
-            pitchDialog.showAndWait().ifPresent(response -> {
-                playScale(Integer.parseInt(response));
-            });
-    }    
-    
-    /**
-     * When the user clicks the "Stop playing" button, stop playing the scale.
-     * @param event the button click event
-     */
-    @FXML 
-    protected void handleStopPlayingButtonAction(ActionEvent event) {
-        player.stop();
-    }    
-    
-    /**
-     * When the user clicks the "Exit" menu item, exit the program.
-     * @param event the menu selection event
-     */
-    @FXML
-    protected void handleExitMenuItemAction(ActionEvent event) {
-        System.exit(0);
-    }
     
     /**
      * Construct the scene and start the application.
