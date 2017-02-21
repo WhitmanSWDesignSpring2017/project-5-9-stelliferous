99fbe1ca444b81e464152268c679695d32b70e66
Tyler <mauletj@whitman.edu>
2017-02-20 16:08:11 -0800

    Added on-click rectangles, added MidiEvents corresponding to rectangles Added HotKeys to start and stop MidiComposition, to exit window

--- src/tunecomposer/TuneComposer.java
+++ src/tunecomposer/TuneComposer.java
@@ -3,9 +3,6 @@
  */
 package tunecomposer;
 
-import java.awt.MouseInfo;
-import java.awt.Point;
-import java.awt.PointerInfo;
 import java.io.IOException;
 import javafx.application.Application;
 import javafx.event.ActionEvent;
@@ -17,6 +14,16 @@ import javafx.scene.input.MouseEvent;
 import javafx.stage.Stage;
 import javafx.stage.WindowEvent;
 
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyCodeCombination;
+import javafx.scene.input.KeyCombination;
+import javafx.scene.input.KeyEvent;
+import javafx.scene.layout.StackPane;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Rectangle;
+
 /**
  * This JavaFX application lets the user play scales.
  * @author Janet Davis 
@@ -24,6 +31,22 @@ import javafx.stage.WindowEvent;
  * @since January 26, 2017
  */
 public class TuneComposer extends Application {
+
+    //creates a MidiPlayer object with 100 ticks/beat, 1 beat/second
+    public MidiPlayer MidiComposition = new MidiPlayer(100,60);
+    
+    //sets, volume, duration, channel, and trackIndex for the MidiPlayer's notes
+    final int volume = 50;
+    final int duration = 100;
+    final int channel = 0;
+    final int trackIndex = 0;
+    
+    final KeyCombination playKey = new KeyCodeCombination(KeyCode.P, 
+            KeyCombination.CONTROL_DOWN);
+    final KeyCombination stopKey = new KeyCodeCombination(KeyCode.S, 
+            KeyCombination.CONTROL_DOWN);
+    final KeyCombination exitKey = new KeyCodeCombination(KeyCode.Q, 
+            KeyCombination.CONTROL_DOWN);
     
     /**
      * Construct the scene and start the application.
@@ -42,17 +65,62 @@ public class TuneComposer extends Application {
         });        
         primaryStage.show();
     }
+    
+    @FXML StackPane rectStackPane;
+    
+    @FXML 
+    public void gridClick(MouseEvent e) throws IOException{
+        int yCoordinate = (int)e.getY();
+        int yPitch = 127-yCoordinate/10;
+        int xCoordinate = (int)e.getX();
+        System.out.println(xCoordinate + ", " + yCoordinate + ": "+yPitch);
+        MidiComposition.addNote(yPitch, volume, xCoordinate,
+                                    duration, channel, trackIndex);  
+        Rectangle rect = new Rectangle();
+        rect.setTranslateX(xCoordinate-1000+9);
+        rect.setTranslateY((yCoordinate/10)*10-640+5);
+        rect.setHeight(10);
+        rect.setWidth(20);
+        rect.setFill(Color.BLACK);
+        rectStackPane.getChildren().add(rect);
+    };
 
     @FXML
-    public void gridClick(MouseEvent e){
-        int yCoordinate = (int)e.getY();
-        int xCoordinate = (int)e.getX();
-        System.out.println(xCoordinate + ", " + yCoordinate);
+    public void handleExitAction(ActionEvent e){
+        System.exit(0);
+    }
+    
+    @FXML
+    public void handlePlayAction(ActionEvent e){
+        System.out.println("Playing");
+        MidiComposition.play();
+    }
+    
+    @FXML
+    public void handleStopAction(ActionEvent e){
+        System.out.println("Stopping");
+        MidiComposition.stop();
+    }
+    
+    @FXML
+    public void handleHotKeyAction(KeyEvent e){
+        if (playKey.match(e)){
+            System.out.println("Control P");
+            MidiComposition.play();
+        } else if (stopKey.match(e)){
+            System.out.println("Control S");
+            MidiComposition.stop();
+        } else if (exitKey.match(e)){
+            System.out.println("Control Q");
+            System.exit(0);
+        }
+    }
+    
+    
+    public void initialize(URL location, ResourceBundle resources) {
+    // TODO Auto-generated method stub
 
-    };
-    
-    
-    
+}
     /**
      * Launch the application.
      * @param args the command line arguments are ignored
@@ -60,5 +128,5 @@ public class TuneComposer extends Application {
     public static void main(String[] args) {
         launch(args);
     }
-    
+   
 }
