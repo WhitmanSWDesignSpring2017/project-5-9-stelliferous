b2c31fad0f1f6b35022975db57c7dd4584ea5628
mauletj <mauletj@emilpost.whitman.edu>
2017-03-06 21:10:06 -0800

    added comments, javadocs

--- src/controller/NoteRectangle.java
+++ src/controller/NoteRectangle.java
@@ -11,179 +11,183 @@ import javafx.scene.paint.Color;
 import javafx.scene.shape.Rectangle;
 
 /**
- *
+ * A NoteRectangle object, which is used to record and display notes on user
+ * creation. Includes a rectangle, to be added to the AnchorPane and visually
+ * display the rectangle. Includes channel, color, and stroke of the rectangle.
  * @author wangj2
+ * @author mauletj
  */
 public class NoteRectangle {
-    public Rectangle Notes;
+    
+    //attributes to hold a NoteRectangle's Rectangle, channel, color, and
+    //associated instrument
+    protected Rectangle Notes;
     private int channel;
     private Color rectColor;
-    private int instrument;
+    private final int instrument;
     
-            
+    /**
+     * Initializer for the NoteRectangle object
+     * @param x x-coordinate of upper-left hand corner of the rectangle
+     * @param y y-coordinate of upper-left hand corner of the rectangle
+     * @param color fill color of the rectangle
+     * @param channel channel number of the rectangle
+     * @param instrument instrument number of the rectangle
+     */        
     public NoteRectangle(double x, int y, Color color, int channel, int instrument) {
+        //assigns user-given attribute values of instrument, channel, color
         this.instrument = instrument;
         this.channel = channel;
         this.rectColor = color;
+        
+        //creates a new rectangle object for visual representation
         Notes = new Rectangle(x,y,100,10);
         Notes.setFill(rectColor);
         Notes.setStroke(Color.CRIMSON);
         Notes.setStrokeWidth(2);
     }
     
+    /**
+     * returns value of the NoteRectangle object's 'instrument' attribute
+     * @return instrument
+     */
     public int getInstrument() {
         return instrument;
     }
+    
+    /**
+     * returns value of the NoteRectangle object's 'channel' attribute
+     * @return channel
+     */
     public int getChannel() {
         return channel;
     }
     
-    public Color getColor() {
-        return rectColor;
-    }
+    /**
+     * sets value of the NoteRectangle object's 'rectColor' attribute
+     * @param newColor takes user's given Color
+     */
     public void setColor(Color newColor) {
         rectColor = newColor;
     }
     
+    /**
+     * sets value of the NoteRectangle object's 'channel' attribute
+     * @param newChannel takes user's given Channel (an integer)
+     */
     public void setChannel(int newChannel) {
         channel = newChannel;
     }
     
+    /**
+     * Set the MousePressed event for the Rectangle Notes
+     * @param mouseEvent an incoming event, when the mouse is pressed
+     */
     public void setOnMousePressed(EventHandler<MouseEvent> mouseEvent) {
         Notes.setOnMousePressed(mouseEvent);
     }
     
+    /**
+     * Sets the MouseDragged event for the Rectangle Notes
+     * @param mouseEvent an incoming event, when the mouse is dragged
+     */
     public void setOnMouseDragged(EventHandler<MouseEvent> mouseEvent) {
         Notes.setOnMouseDragged(mouseEvent);
     }
     
+    /**
+     * Sets the MouseReleased event for the Rectangle Notes
+     * @param mouseEvent an incoming event, when the mouse is released
+     */
     public void setOnMouseReleased(EventHandler<MouseEvent> mouseEvent) {
         Notes.setOnMouseReleased(mouseEvent);
     }
- 
+    
+    /**
+     * Sets the Mouse Clicked event for the Rectangle Notes
+     * @param mouseEvent an incoming event, when the mouse is clicked
+     */
     public void setOnMouseClicked(EventHandler<MouseEvent> mouseEvent) {
         Notes.setOnMouseClicked(mouseEvent);
     }
 
+    /**
+     * Allows the user to set the stroke color of a Rectangle Note
+     * @param newcolor the color given by the user (should be crimson/black)
+     */
     public void setStroke(Color newcolor) {
         Notes.setStroke(newcolor);
     }
     
+    /**
+     * Returns the x-coordinate of a Rectangle Note
+     * @return Notes.getX() the x-coordinate
+     */
     public double getX() {
         return Notes.getX();
     }
     
+    /**
+     * Returns the y-coordinate of a Rectangle Note
+     * @return Notes.getY() the y-coordinate
+     */
     public double getY() {
         return Notes.getY();
     }
     
+    /**
+     * Returns the width of a Rectangle Note
+     * @return Notes.getWidth(), the width
+     */
     public double getWidth() {
         return Notes.getWidth();
     }
     
+    /**
+     * Returns the height of a Rectangle Note
+     * @return Notes.getHeight(), the height
+     */
     public double getHeight() {
         return Notes.getHeight();
     }
     
+    /**
+     * Sets the width of a Rectangle Note
+     * @param width the width value given by the user
+     */
     public void setWidth(double width) {
         Notes.setWidth(width);
     }        
     
+    /**
+     * Sets the height of a Rectangle Note
+     * @param height the height value given by the user
+     */
     public void setHeight(double height) {
         Notes.setHeight(height);
     }
     
+    /**
+     * Sets the upper-left hand corner x-coordinate of a Rectangle note
+     * @param newX the x-coordinate double value given by the user
+     */
     public void setX(double newX) {
         Notes.setX(newX);
     }
     
+    /**
+     * Sets the upper-left hand corner y-coordinate of a Rectangle note
+     * @param newY the y-coordinate double value given by the user
+     */
     public void setY(double newY) {
         Notes.setY(newY);
     }
     
+    /**
+     * Sets the amount by which the Rectangle Note is translated in y-direction
+     * @param translateY the translation in the y-direction given by the user
+     */
     public void setTranslateY(double translateY) {
         Notes.setTranslateY(translateY);
     }
-    
-    /* 
-    //private double orgSceneX, orgSceneY;
-    private ArrayList<Double> orgTranslateXs = new ArrayList<>();
-    private ArrayList<Double> orgTranslateYs = new ArrayList<>();
-    private ArrayList<Double> orgWidths = new ArrayList<>();
-    private boolean stretch;
-            
-    EventHandler<MouseEvent> circleOnMousePressedEventHandler = 
-        new EventHandler<MouseEvent>() {
- 
-        @Override
-        public void handle(MouseEvent t) {
-            reset_coordinates(t);
-            //orgSceneX = t.getX();
-            //orgSceneY = t.getY();
-            //Rectangle currentRect = (Rectangle) t.getSource();
-            for (int i=0; i<SELECTED_NOTES.size();i++) {
-                orgTranslateXs.add(SELECTED_NOTES.get(i).getX());
-                orgTranslateYs.add(SELECTED_NOTES.get(i).getY());
-                orgWidths.add(SELECTED_NOTES.get(i).getWidth());
-            }
-        }
-    };
-     
-    EventHandler<MouseEvent> circleOnMouseDraggedEventHandler = 
-        new EventHandler<MouseEvent>() {
- 
-        @Override
-        public void handle(MouseEvent t) {
-            double offsetX = t.getX() - xCoordinate; //used to be orgSceneX
-            double offsetY = t.getY() - yCoordinate; //used to be orgSceneY
-
-            for (int i=0; i<SELECTED_NOTES.size();i++) {
-                if ( (xCoordinate >= (orgTranslateXs.get(i) //used to be orgSceneX
-                                    +SELECTED_NOTES.get(i).getWidth()-5)
-                        &&
-                     xCoordinate <= (orgTranslateXs.get(i) //used to be orgSceneX
-                                   +SELECTED_NOTES.get(i).getWidth()))
-                        && 
-                        (yCoordinate >= orgTranslateYs.get(i) //used to be orgSceneY
-                        && yCoordinate <= (orgTranslateYs.get(i)+10)) //used to be orgSceneY
-                   )
-                {
-                    stretch = true;
-                    System.out.println("stetch");
-                }
-            }
-            
-            for (int i=0; i<SELECTED_NOTES.size();i++) {
-                if (stretch) {
-                    double width = orgWidths.get(i);
-                    SELECTED_NOTES.get(i).setWidth(width+offsetX);
-                } else {
-                    double newTranslateX = orgTranslateXs.get(i) + offsetX;
-                    double newTranslateY = orgTranslateYs.get(i) + offsetY;
-                    SELECTED_NOTES.get(i).setX(newTranslateX);
-                    SELECTED_NOTES.get(i).setY(newTranslateY);
-                }
-            }
-        }
-    };
-        EventHandler<MouseEvent> circleOnMouseReleasedEventHandler = 
-        new EventHandler<MouseEvent>() {
- 
-        @Override
-        public void handle(MouseEvent t) {
-            stretch = false;
-            orgTranslateXs.clear();
-            orgTranslateYs.clear();
-            orgWidths.clear();
-            reset_coordinates(t);
-            for (int i=0; i<SELECTED_NOTES.size(); i++) {
-                double currentY = SELECTED_NOTES.get(i).getY();
-                double finalY = ((int)(currentY/10))*10;
-                double offset = finalY - currentY;
-                SELECTED_NOTES.get(i).setTranslateY(offset);
-            }
-        }
-    }; 
-    }
-*/
 }
