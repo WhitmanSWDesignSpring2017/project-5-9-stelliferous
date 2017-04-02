a0b66503e2538c4268c0f870990302bd4c178d27
Kaylin <Kaylin@10.25.10.246>
2017-04-01 17:49:42 -0700

    tunecomposernoteselected.java edits (still need to finish up docs after refactoring)

--- src/controller/TuneComposerNoteSelection.java
+++ src/controller/TuneComposerNoteSelection.java
@@ -23,12 +23,10 @@ import javax.sound.midi.ShortMessage;
 
 
 /**
- * This JavaFX application lets the user compose tunes by clicking!
- * @author Janet Davis 
- * @author Kai McConnell
- * @author Jingyuan Wang
+ * This class allows for note creation, selection, editing, and deletion.
  * @author Tyler Maule
- * @since January 26, 2017
+ * @author Jingyuan Wang
+ * @author Kaylin Jarriel
  */
 public class TuneComposerNoteSelection {
     
