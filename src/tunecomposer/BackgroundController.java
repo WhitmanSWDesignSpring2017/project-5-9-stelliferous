f5514235d6e483913b368b2a7dfd08bfb6f9dada
Jingyuan Wang <wangj2@whitman.edu>
2017-05-12 17:33:40 -0700

    added the color picker feature

--- src/tunecomposer/BackgroundController.java
+++ src/tunecomposer/BackgroundController.java
@@ -7,6 +7,7 @@ import javafx.fxml.Initializable;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.layout.AnchorPane;
+import javafx.scene.paint.Color;
 
 /**
  * The FXML Controller class to draw grey lines in the canvas of 
@@ -23,26 +24,47 @@ public class BackgroundController implements Initializable{
     
     //makes available the background from TuneComposerBackground
     @FXML AnchorPane backgroundPane;
-
+    
+    //makes available the graphicsContext
+    GraphicsContext gc;
+    
     /**
-     * Initializes the controller class. Draws grey lines on the Canvas named
-     * "lines"
+     * Initializes the controller class. 
      * @param url placeholder/default/generic url
      * @param rb placeholder/default/generic resource bundle
      */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         //set up for drawing grey lines
-        GraphicsContext gc = linesCanvas.getGraphicsContext2D();
+        gc = linesCanvas.getGraphicsContext2D();
+        drawLine(Color.BLACK);
+    }
+
+    /**
+     * Initializes the controller class. Draws grey lines on the Canvas named
+     * "lines"
+     */
+    private void drawLine(Color value) {
         gc.setLineWidth(1.0);
-        
+        //backgroundPane.setStyle("-fx-background-color: " + "rgb(" +.getRed()*256 + "," +Color.BLUE.getGreen()*256 + "," + Color.BLUE.getBlue()*256 + ")");
+        //backgroundPane.setStyle("-fx-background-color: BLUE");
         //for loop to draw lines
         for (int y = 0; y < 1280; y+=10) {
             double y1 ;
             y1 = y + 0.5;
             gc.moveTo(0, y1);
+            gc.setStroke(value);
             gc.lineTo(8000, y1);
             gc.stroke();
         }
-    }    
+    }
+    
+    /**
+     * Change the color of the background and the lines are the invert color of the background.
+     * @param value the color from the color picker
+     */
+    protected void changeBackgroundColor(Color value) {
+        drawLine(value.invert());
+        backgroundPane.setStyle("-fx-background-color: " + "rgb(" +value.getRed()*256 + "," +value.getGreen()*256 + "," + value.getBlue()*256 + ")");
+    }
 }
